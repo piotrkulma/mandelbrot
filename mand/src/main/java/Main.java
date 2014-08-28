8282026abd956f16899c31f0af6135d272548b30
peter <piotrkulma@gmail.com>
2014-08-28 22:35:18 +0200

    Beta 1.0

---
+++ mand/src/main/java/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Piotr Kulma on 28.08.14.
+ */
+public class Main {
+    public static void main(String... args) {
+        new MandForm();
+    }
+}
---
+++ mand/src/main/java/MandForm.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Piotr Kulma on 28.08.14.
+ */
+public class MandForm extends JFrame{
+    private MandPanel panel;
+
+    public MandForm() {
+        super("Mandelbrot set");
+        initForm();
+        initAndAddPanel();
+    }
+
+    private void initAndAddPanel() {
+        panel = new MandPanel();
+        this.getContentPane().add(panel, BorderLayout.CENTER);
+    }
+
+    private void initForm() {
+        this.setSize(800, 700);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+}
